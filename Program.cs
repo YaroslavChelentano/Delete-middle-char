e7e0661207b5d4fecad1b5f87cc073d48d0550b1
YaroslavChelentano <49638499+YaroslavChelentano@users.noreply.github.com>
2019-07-04 12:16:35 +0300

    Solution

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = Console.ReadLine();
+            string[] words = text.Split(new char[] { ' ' });
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                
+                if(words[i].Length%2!=0)
+                {
+                    int middleOfNumber = 0;
+                    middleOfNumber = (words[i].Length / 2);
+                    words[i]=words[i].Remove(middleOfNumber, 1 );
+
+                }
+                Console.Write(words[i] + " " );
+
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
